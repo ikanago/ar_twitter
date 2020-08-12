e1b49abf010168bc266fe82b6bd345aec75012f0
ikanago <28985004+ikanago@users.noreply.github.com>
2020-08-12 17:06:48 +0900

    Build simple ping server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello");
+import express from "express";
+const app = express();
+
+const port = 8080
+const server = app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
+});
+
+const payload = {
+    tweets: "Pong",
+};
+
+app.get("/ping", (_req, res, _next) => {
+    res.json(payload);
+})
