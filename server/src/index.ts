e0525fceab2551762ab1a7069e16ebf4d0ef758d
ikanago <28985004+ikanago@users.noreply.github.com>
2020-08-13 15:49:17 +0900

    Access Twitter API to get home timeline

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,38 @@
 import express from "express";
-const app = express();
+import Twitter from "twitter";
+import { getHomeTimeline } from "./accessTwitterAPI.js";
 
-const port = 8080
+const app = express();
+const client = new Twitter ({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+const port = 8080;
 const server = app.listen(port, () => {
     console.log(`Server is listening to port ${port}`);
 });
 
-const payload = {
-    tweets: "Pong",
-};
+app.get("/home_timeline", (req, res, _next) => {
+    let count = NaN;
+    if (req.query && typeof req.query.count === "string") {
+        count = parseInt(req.query.count);
+        if (isNaN(count)) {
+            return res.status(422).json({
+                error: "count must be integer",
+            });
+        }
+    }
 
-app.get("/ping", (_req, res, _next) => {
-    res.json(payload);
+    getHomeTimeline(client, (req.query as any).name)
+        .then(body => {
+            res.json(body);
+        })
+        .catch(err => {
+            console.error(err);
+            res.sendStatus(404);
+        });
 })
+
