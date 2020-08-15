a2ffd760a42b2dcc4ff963b4d2be8e5a05c33867
ikanago <28985004+ikanago@users.noreply.github.com>
2020-08-16 02:04:59 +0900

    Rollback

--- server/src/requestHandlers.ts
+++ server/src/requestHandlers.ts
@@ -1,104 +1,28 @@
 import type * as express from "express";
-import oauth from "oauth";
-import { v4 as uuidv4 } from "uuid";
+import type Twitter from "twitter";
+import { getHomeTimeline } from "./accessTwitterAPI";
 import { ValidationError, validateHomeTimeline } from "./validateRequest";
-import {
-    getOAuthAccessToken,
-    getOAuthRequestToken,
-    getHomeTimeline,
-} from "./oauthHelper";
 
 /**
  * Handle request coming to '/home_timeline'.
  */
-const handleHomeTimeline = (
-    oauthClient: oauth.OAuth,
-    loginCache: Map<string, string>
-): express.RequestHandler => {
+const handleHomeTimeline = (client: Twitter): express.RequestHandler => {
     return (request, response) => {
-        const id: string | undefined = request.cookies.id;
-        if (!id || !loginCache.has(id)) {
-            response.status(401).json({ error: "You are not autherized" });
-            return;
-        }
-        const token = loginCache.get(id);
-        const [oauthAccessToken, oauthAccessTokenSecret] = token.split(":");
         const query = validateHomeTimeline(request);
         if (query instanceof ValidationError) {
+            console.error(query.error);
             response.status(422).json(query);
-            return;
+        } else {
+            getHomeTimeline(client, query.count)
+                .then(body => {
+                    response.json(body);
+                })
+                .catch(err => {
+                    console.error(err);
+                    response.status(500);
+                });
         }
-        const tweets = getHomeTimeline(
-            oauthClient,
-            oauthAccessToken,
-            oauthAccessTokenSecret,
-            query.count
-        );
-        response.json({
-            tweets: tweets,
-        });
     };
 };
 
-/**
- * Obtain request tokens. Redirect user to Twitter's authentication page.
- */
-const handleAuthRequest = (
-    oauthClient: oauth.OAuth
-): express.RequestHandler => {
-    return async (request, response) => {
-        const {
-            oauth_token: oauthToken,
-            oauth_token_secret: oauthTokenSecret,
-            results,
-        } = await getOAuthRequestToken(oauthClient);
-        console.log(oauthToken, oauthTokenSecret);
-        request.session.oauth_token = oauthToken;
-        request.session.oauth_token_secret = oauthTokenSecret;
-        response.redirect(
-            `https://twitter.com/oauth/authenticate?oauth_token=${oauthToken}`
-        );
-    };
-};
-
-/**
- * Handle callback from Twitter API.
- */
-const handleAuthCallback = (
-    oauthClient: oauth.OAuth,
-    loginCache: Map<string, string>
-): express.RequestHandler => {
-    return async (request, response) => {
-        const {
-            oauth_token: oauthToken,
-            oauth_token_secret: oauthTokenSecret,
-        } = request.session;
-        console.log(oauthToken, oauthTokenSecret);
-        const { oauth_verifier: oauthVerifier } = request.query;
-        if (typeof oauthVerifier !== "string") {
-            response.status(500);
-            return;
-        }
-        const {
-            oauth_access_token: oauthAccessToken,
-            oauth_access_token_secret: oauthAccessTokenSecret,
-            results,
-        } = await getOAuthAccessToken(
-            oauthClient,
-            oauthToken,
-            oauthTokenSecret,
-            oauthVerifier
-        );
-        const id = uuidv4();
-        const token = `${oauthAccessToken}:${oauthAccessTokenSecret}`;
-        loginCache.set(id, token);
-        response.cookie("id", id, {
-            maxAge: 6000000,
-            httpOnly: true,
-            path: "/",
-        });
-        response.redirect("https://ar-twitter.netlify.app/app.html");
-    };
-};
-
-export { handleHomeTimeline, handleAuthRequest, handleAuthCallback };
+export { handleHomeTimeline };
